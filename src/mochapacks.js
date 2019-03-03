28abe8b8828f5316d8c04c3b98e4193ebedf9818
Ian Webb <eon.stella.artois@gmail.com>
2019-03-03 21:19:38 +0000

    refactor(mochapacks.js): refactor event handlers as methods

--- src/mochapacks.js
+++ src/mochapacks.js
@@ -7,8 +7,8 @@ const {
     EVENT_RUN_END,
     EVENT_TEST_BEGIN,
     EVENT_TEST_END,
-    EVENT_TEST_FAIL,
     EVENT_TEST_PASS,
+    EVENT_TEST_FAIL,
     EVENT_TEST_PENDING,
     EVENT_TEST_RETRY,
     EVENT_SUITE_BEGIN,
@@ -17,69 +17,32 @@ const {
     EVENT_HOOK_END
 } = Mocha.Runner.constants;
 
-// this reporter outputs test results, indenting two spaces per suite
-class MyReporter {
+/**
+ * @class
+ * @summary this reporter outputs test results, indenting two spaces per suite
+ */
+class MochaPacksReporter {
+    /**
+     * @constructor
+     * @param {Runner} runner
+     */
     constructor(runner) {
         this._indents = 0;
-        const stats = runner.stats;
+        //const stats = runner.stats;
 
-        runner
-            .once(EVENT_RUN_BEGIN, () => {
-                console.log("start");
-            })
-            .on(EVENT_SUITE_BEGIN, suite => {
-                console.log(
-                    `${this.indent()}begin_suite: ${suite.fullTitle()}`
-                );
-                this.increaseIndent();
-            })
-            .on(EVENT_SUITE_END, suite => {
-                console.log(`${this.indent()}end_suite: ${suite.fullTitle()}`);
-                this.decreaseIndent();
-            })
-            .on(EVENT_HOOK_BEGIN, hook => {
-                console.log(`${this.indent()}begin_hook: ${hook.fullTitle()}`);
-            })
-            .on(EVENT_HOOK_END, hook => {
-                console.log(`${this.indent()}end_hook: ${hook.fullTitle()}`);
-            })
-            .on(EVENT_TEST_BEGIN, test => {
-                // Test#fullTitle() returns the suite name(s)
-                // prepended to the test title
-                console.log(`${this.indent()}begin_test: ${test.fullTitle()}`);
-            })
-            .on(EVENT_TEST_END, test => {
-                // Test#fullTitle() returns the suite name(s)
-                // prepended to the test title
-                console.log(`${this.indent()}end_test: ${test.fullTitle()}`);
-            })
-            .on(EVENT_TEST_PASS, test => {
-                // Test#fullTitle() returns the suite name(s)
-                // prepended to the test title
-                console.log(`${this.indent()}pass: ${test.fullTitle()}`);
-            })
-            .on(EVENT_TEST_PENDING, test => {
-                console.log(`${this.indent()}pending: ${test.fullTitle()}`);
-            })
-            .on(EVENT_TEST_FAIL, (test, err) => {
-                console.log(
-                    `${this.indent()}fail: ${test.fullTitle()} - error: ${
-                        err.message
-                    }`
-                );
-            })
-            .on(EVENT_TEST_RETRY, (test, err) => {
-                console.log(
-                    `${this.indent()}retry: ${test.fullTitle()} - error: ${
-                        err.message
-                    }`
-                );
-            })
-            .once(EVENT_RUN_END, () => {
-                console.log(
-                    `end: ${stats.passes}/${stats.passes + stats.failures} ok`
-                );
-            });
+        // event listeners
+        runner.once(EVENT_RUN_BEGIN, this._eventRunBegin.bind(this));
+        runner.once(EVENT_RUN_END, this._eventRunEnd.bind(this));
+        runner.on(EVENT_SUITE_BEGIN, this._eventSuiteBegin.bind(this));
+        runner.on(EVENT_SUITE_END, this._eventSuiteEnd.bind(this));
+        runner.on(EVENT_HOOK_BEGIN, this._eventHookBegin.bind(this));
+        runner.on(EVENT_HOOK_END, this._eventHookEnd.bind(this));
+        runner.on(EVENT_TEST_BEGIN, this._eventTestBegin.bind(this));
+        runner.on(EVENT_TEST_END, this._eventTestEnd.bind(this));
+        runner.on(EVENT_TEST_PASS, this._eventTestPass.bind(this));
+        runner.on(EVENT_TEST_FAIL, this._eventTestFail.bind(this));
+        runner.on(EVENT_TEST_PENDING, this._eventTestPending.bind(this));
+        runner.on(EVENT_TEST_RETRY, this._eventTestRetry.bind(this));
     }
 
     indent() {
@@ -93,6 +56,122 @@ class MyReporter {
     decreaseIndent() {
         this._indents--;
     }
+
+    /**
+     * @method eventRunBegin
+     * @private
+     */
+    _eventRunBegin() {
+        console.log(`${this.indent()}begin_run`);
+    }
+
+    /**
+     * @method eventRunEnd
+     * @private
+     */
+    _eventRunEnd() {
+        console.log(`${this.indent()}end_run`);
+    }
+
+    /**
+     * @method eventSuiteBegin
+     * @param {Mocha.Suite} suite
+     * @private
+     */
+    _eventSuiteBegin(suite) {
+        console.log(`${this.indent()}begin_suite: ${suite.fullTitle()}`);
+        this.increaseIndent();
+    }
+
+    /**
+     * @method eventSuiteEnd
+     * @param {Mocha.Suite} suite
+     * @private
+     */
+    _eventSuiteEnd(suite) {
+        console.log(`${this.indent()}end_suite: ${suite.fullTitle()}`);
+        this.decreaseIndent();
+    }
+
+    /**
+     * @method eventHookBegin
+     * @param {Mocha.Hook} hook
+     * @private
+     */
+    _eventHookBegin(hook) {
+        console.log(`${this.indent()}begin_hook: ${hook.fullTitle()}`);
+    }
+
+    /**
+     * @method eventHookEnd
+     * @param {Mocha.Hook} hook
+     * @private
+     */
+    _eventHookEnd(hook) {
+        console.log(`${this.indent()}end_hook: ${hook.fullTitle()}`);
+    }
+
+    /**
+     * @method eventTestBegin
+     * @param {Mocha.Test} test
+     * @private
+     */
+    _eventTestBegin(test) {
+        // Test#fullTitle() returns the suite name(s)
+        // prepended to the test title
+        console.log(`${this.indent()}begin_test: ${test.fullTitle()}`);
+    }
+
+    /**
+     * @method eventTestEnd
+     * @param {Mocha.Test} test
+     * @private
+     */
+    _eventTestEnd(test) {
+        console.log(`${this.indent()}end_test: ${test.fullTitle()}`);
+    }
+
+    /**
+     * @method eventTestPass
+     * @param {Mocha.Test} test
+     * @private
+     */
+    _eventTestPass(test) {
+        console.log(`${this.indent()}pass: ${test.fullTitle()}`);
+    }
+
+    /**
+     * @method eventTestFail
+     * @param {Mocha.Test} test
+     * @param {Error} err
+     * @private
+     */
+    _eventTestFail(test, err) {
+        console.log(
+            `${this.indent()}fail: ${test.fullTitle()} - error: ${err.message}`
+        );
+    }
+
+    /**
+     * @method eventTestPending
+     * @param {Mocha.Test} test
+     * @private
+     */
+    _eventTestPending(test) {
+        console.log(`${this.indent()}pending: ${test.fullTitle()}`);
+    }
+
+    /**
+     * @method eventTestRetry
+     * @param {Mocha.Test} test
+     * @param {Error} err
+     * @private
+     */
+    _eventTestRetry(test, err) {
+        console.log(
+            `${this.indent()}retry: ${test.fullTitle()} - error: ${err.message}`
+        );
+    }
 }
 
-module.exports = MyReporter;
+module.exports = MochaPacksReporter;
