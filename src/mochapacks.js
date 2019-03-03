5de93c30c8b5b3c8711dd6fa43e5fbd4c8518fb7
Ian Webb <eon.stella.artois@gmail.com>
2019-03-03 19:10:17 +0000

    refactor(mochapacks.js): Changed reporter to a class
    Created a class based reporter with all the necessary event types

--- src/mochapacks.js
+++ src/mochapacks.js
@@ -1,23 +1,98 @@
-// mochapacks.js
-var mocha = require("mocha");
-module.exports = MyReporter;
+// my-reporter.js
+"use strict";
 
-function MyReporter(runner) {
-    mocha.reporters.Base.call(this, runner);
-    var passes = 0;
-    var failures = 0;
+const Mocha = require("mocha");
+const {
+    EVENT_RUN_BEGIN,
+    EVENT_RUN_END,
+    EVENT_TEST_BEGIN,
+    EVENT_TEST_END,
+    EVENT_TEST_FAIL,
+    EVENT_TEST_PASS,
+    EVENT_TEST_PENDING,
+    EVENT_TEST_RETRY,
+    EVENT_SUITE_BEGIN,
+    EVENT_SUITE_END,
+    EVENT_HOOK_BEGIN,
+    EVENT_HOOK_END
+} = Mocha.Runner.constants;
 
-    runner.on("pass", function(test) {
-        passes++;
-        console.log("pass: %s", test.fullTitle());
-    });
+// this reporter outputs test results, indenting two spaces per suite
+class MyReporter {
+    constructor(runner) {
+        this._indents = 0;
+        const stats = runner.stats;
 
-    runner.on("fail", function(test, err) {
-        failures++;
-        console.log("fail: %s -- error: %s", test.fullTitle(), err.message);
-    });
+        runner
+            .once(EVENT_RUN_BEGIN, () => {
+                console.log("start");
+            })
+            .on(EVENT_SUITE_BEGIN, suite => {
+                console.log(
+                    `${this.indent()}begin_suite: ${suite.fullTitle()}`
+                );
+                this.increaseIndent();
+            })
+            .on(EVENT_SUITE_END, suite => {
+                console.log(`${this.indent()}end_suite: ${suite.fullTitle()}`);
+                this.decreaseIndent();
+            })
+            .on(EVENT_HOOK_BEGIN, hook => {
+                console.log(`${this.indent()}begin_hook: ${hook.fullTitle()}`);
+            })
+            .on(EVENT_HOOK_END, hook => {
+                console.log(`${this.indent()}end_hook: ${hook.fullTitle()}`);
+            })
+            .on(EVENT_TEST_BEGIN, test => {
+                // Test#fullTitle() returns the suite name(s)
+                // prepended to the test title
+                console.log(`${this.indent()}begin_test: ${test.fullTitle()}`);
+            })
+            .on(EVENT_TEST_END, test => {
+                // Test#fullTitle() returns the suite name(s)
+                // prepended to the test title
+                console.log(`${this.indent()}end_test: ${test.fullTitle()}`);
+            })
+            .on(EVENT_TEST_PASS, test => {
+                // Test#fullTitle() returns the suite name(s)
+                // prepended to the test title
+                console.log(`${this.indent()}pass: ${test.fullTitle()}`);
+            })
+            .on(EVENT_TEST_PENDING, test => {
+                console.log(`${this.indent()}pending: ${test.fullTitle()}`);
+            })
+            .on(EVENT_TEST_FAIL, (test, err) => {
+                console.log(
+                    `${this.indent()}fail: ${test.fullTitle()} - error: ${
+                        err.message
+                    }`
+                );
+            })
+            .on(EVENT_TEST_RETRY, (test, err) => {
+                console.log(
+                    `${this.indent()}retry: ${test.fullTitle()} - error: ${
+                        err.message
+                    }`
+                );
+            })
+            .once(EVENT_RUN_END, () => {
+                console.log(
+                    `end: ${stats.passes}/${stats.passes + stats.failures} ok`
+                );
+            });
+    }
 
-    runner.on("end", function() {
-        console.log("end: %d/%d", passes, passes + failures);
-    });
+    indent() {
+        return Array(this._indents).join("  ");
+    }
+
+    increaseIndent() {
+        this._indents++;
+    }
+
+    decreaseIndent() {
+        this._indents--;
+    }
 }
+
+module.exports = MyReporter;
--- test/test1.spec.js
+++ test/test1.spec.js
@@ -1,6 +1,16 @@
 const expect = require("chai").expect;
 
 describe("test1", function() {
+    this.retries(2);
+
+    before(function() {
+        let a = 1;
+    });
+
+    after(function() {
+        let b = 1;
+    });
+
     it("should test that 1 equals 1", function() {
         expect(1).to.equal(1);
     });
@@ -10,4 +20,5 @@ describe("test1", function() {
     it("should test that 3 equals 3", function() {
         expect(1).to.equal(3);
     });
+    it("should make a pending test");
 });
