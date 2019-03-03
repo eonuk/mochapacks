5c492d112b1756e78e7019d5d49dbbe9b2e0151b
Ian Webb <eon.stella.artois@gmail.com>
2019-03-03 15:19:33 +0000

    feat: basic reporter
    a basic custom reporter with example test and associated build tools

--- src/main.js
+++
@@ -1,7 +0,0 @@
-export default class Main {
-    constructor() {
-        let a = 1;
-        let b = 2;
-        return a + b;
-    }
-}
---
+++ test/test1.spec.js
@@ -0,0 +1,13 @@
+const expect = require("chai").expect;
+
+describe("test1", function() {
+    it("should test that 1 equals 1", function() {
+        expect(1).to.equal(1);
+    });
+    it("should test that 2 equals 2", function() {
+        expect(2).to.equal(2);
+    });
+    it("should test that 3 equals 3", function() {
+        expect(1).to.equal(3);
+    });
+});
